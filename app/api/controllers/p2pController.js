0441e40e1dfdb798083d644af5afc4b39904ffd7
abdul <Ainzburn@gmail.com>
2023-07-05 13:40:58 +0800

    add delete contract func

--- app/api/controllers/p2pController.js
+++ app/api/controllers/p2pController.js
@@ -6,58 +6,11 @@ export const addP2PContract = async (req, res) => {
     req.headers
   );
   if (p2p_contract.status === "INPUT_QUERY_SUCCESS") {
-    res.status(201).json({
-      message: "CONTRACT_ADDED",
-      details: {
-        wallet_balance: {
-          USD: Number(p2p_contract.wallet_balance[0].amount.toFixed(2)),
-          BTC: p2p_contract.wallet_balance[1].amount,
-          ETH: p2p_contract.wallet_balance[2].amount,
-        },
-      },
-    });
+    res.status(201).json({ message: "CONTRACT_ADDED" });
   } else if (p2p_contract.status === "INPUT_QUERY_FAILURE") {
     res.status(500).json({ message: "CONTRACT_CREATION_ERROR" });
   } else if (p2p_contract.status === "BAD_REQUEST") {
     res.status(400).json({ message: "REQUEST_ERROR" });
-  } else if (p2p_contract.status === "UPDATE_QUERY_FAILURE") {
-    res.status(500).json({ message: "CONTRACT_CREATION_ERROR" });
-  } else if (p2p_contract.status === "INSUFFICIENT_COIN_AMOUNT") {
-    res.status(400).json({ message: "INSUFFICIENT_COIN_BALANCE" });
-  }
-};
-
-export const getAllOpenContracts = async (req, res) => {
-  let get_all_openContracts = await P2PModel.getOpenContractsModel();
-  if (get_all_openContracts.status === "SELECT_QUERY_SUCCESS") {
-    res.status(200).json({
-      message: "SUCCESS",
-      details: get_all_openContracts.data,
-    });
-  } else if (get_all_openContracts.status === "SELECT_QUERY_FAILURE") {
-    res.status(500).json({
-      message: "FETCHING_CONTRACTS_FAILED",
-    });
-  }
-};
-
-export const getOngoingContracts = async (req, res) => {
-  let get_ongoing_contracts = await P2PModel.getOngoingContractsModel(
-    req.headers
-  );
-  if (get_ongoing_contracts.status === "SELECT_QUERY_SUCCESS") {
-    res.status(200).json({
-      message: "SUCCESS",
-      details: get_ongoing_contracts.data,
-    });
-  } else if (get_ongoing_contracts.status === "SELECT_QUERY_FAILURE") {
-    res.status(500).json({
-      message: "FETCHING_ONGOING_FAILED",
-    });
-  } else if (get_ongoing_contracts.status === "BAD_REQUEST") {
-    res.status(400).json({
-      message: "REQUEST_ERROR",
-    });
   }
 };
 
@@ -65,6 +18,29 @@ export async function buyContract(req, res) {
   try {
     let response = await P2PModel.buyContract(req.headers, req.body);
     if (response.message == "CONTRACT_PURCHASE_SUCCESFUL") {
+      return res.status(200).json({
+        message: response,
+      });
+    } else if (response.message == "BAD_REQUEST") {
+      return res.status(400).json({
+        message: response,
+      });
+    } else {
+      return res.status(500).json({
+        message: response,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+    });
+  }
+}
+
+export async function deleteContract(req, res) {
+  try {
+    let response = await P2PModel.deleteContract(req.headers, req.body);
+    if (response.message == "CONTRACT_DELETED") {
       return res.status(200).json({
         message: response,
       });
